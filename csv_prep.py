d49448954f08149f13a319c2207aad6074c6de16
somin <shimsomin20@gmail.com>
2022-01-06 17:58:52 +0900

    fixed untracked files
    Former-commit-id: 02b2a4723cc6067e4aef74a0ee99041972bbef70

--- csv_prep.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import Mecab
-
-data = pd.read_csv("data/NetflixViewingHistory.csv")
-data.head()
---
+++ readme.md
@@ -0,0 +1,6 @@
+## data
+### raw_data
+-----
+|contents.csv
+- naver_movie_reviews.csv
+- naver_movie_norm_reviews.csv
